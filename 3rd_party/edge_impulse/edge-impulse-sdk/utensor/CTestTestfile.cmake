1faedc77610a279c15a9eab02dfdb535e55c4079
Jan Jongboom <janjongboom@gmail.com>
2020-06-23 09:43:54 +0200

    Add SDK

---
+++ 3rd_party/edge_impulse/edge-impulse-sdk/utensor/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/janjongboom/repos/edgeimpulse/selenium-tests/sanity-check-utensor/edge-impulse-sdk/utensor
+# Build directory: /Users/janjongboom/repos/edgeimpulse/selenium-tests/sanity-check-utensor/edge-impulse-sdk/utensor
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
